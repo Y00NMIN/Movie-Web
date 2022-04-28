d9ba74c4385b108c657c9286775330b6f9fd5f13
ysmgg1@naver.com <yoonmin.tech@gmail.com>
2022-04-28 14:17:27 +0900

    second

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/movie">
+        <Route path="/movie detail">
           <Detail />
         </Route>
         <Route path="/">
