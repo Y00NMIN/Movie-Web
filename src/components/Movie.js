7c2200a927339558e18eefc0914a7f81f0d070cc
ysmgg1@naver.com <yoonmin.tech@gmail.com>
2022-04-28 15:45:17 +0900

    chore: Loading bar

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,29 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to="/movie">{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.movie}>
+      <img className={styles.movie_img} src={coverImg} alt={title} />
+      <div className={styles.movie_desc}>
+        <h2 className={styles.movie_title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie_year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0,235)}...` : summary}</p>
+        <ul className={styles.movie_list}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
